f81986a577175d2957b5bd699fb9937ec3b5b0c7
Александр <darkginesys@gmail.com>
2018-11-10 14:53:26 +0300

    add file

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,17 @@
+
+Vue.component('messages-list', {
+    props: ['messages'],
+    template: '<div><div v-for="message in messages">{{ message.text }}</div></div>'
+});
+
+var app = new Vue({
+    el: '#app',
+    template: '<messages-list :messages="messages" />',
+    data: {
+        messages: [
+            { id: '123', text: 'Wow' },
+            { id: '23', text: 'More' },
+            { id: '3', text: 'Another' }
+        ]
+    }
+});
